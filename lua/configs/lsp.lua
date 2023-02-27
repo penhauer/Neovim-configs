cb949ee6da2d257ccd9a6a1381d8aa2812f70d5c
amirmohammad ghasemi <amirmohammad.ghas3mi@gmail.com>
2023-02-27 14:28:03 +0330

    Move all lsp and cmp related configs to one file. Remvoe gopls abd bashls config

--- lua/configs/lsp.lua
+++ lua/configs/lsp.lua
@@ -3,31 +3,13 @@ require("mason").setup()
 require("mason-lspconfig").setup()
 
 
--- gopls lsp config 
-lspconfig = require "lspconfig"
-util = require "lspconfig/util"
+local lspconfig = require "lspconfig"
+local util = require "lspconfig/util"
 
-lspconfig.gopls.setup {
-  cmd = {"gopls", "serve"},
-  filetypes = {"go", "gomod"},
-  root_dir = util.root_pattern("go.work", "go.mod", ".git"),
-  settings = {
-    gopls = {
-      analyses = {
-        unusedparams = true,
-      },
-      staticcheck = true,
-    },
-  },
-}
-
-
-
--- lsp config
 
 -- Mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
-local opts = { noremap=true, silent=true }
+local opts = { noremap = true, silent = true }
 vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
@@ -41,7 +23,7 @@ local on_attach = function(client, bufnr)
 
   -- Mappings.
   -- See `:help vim.lsp.*` for documentation on any of the below functions
-  local bufopts = { noremap=true, silent=true, buffer=bufnr }
+  local bufopts = { noremap = true, silent = true, buffer = bufnr }
   vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
   -- vim.api.nvim_set_keymap('n', 'gd', vim.lsp.buf.definition(), bufopts)
   vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
@@ -60,90 +42,138 @@ local on_attach = function(client, bufnr)
 end
 
 local lsp_flags = {
-  -- This is the default in Nvim 0.7+
-  debounce_text_changes = 150,
+    -- This is the default in Nvim 0.7+
+    debounce_text_changes = 150,
 }
+
 -- require('lspconfig')['pyright'].setup{
 --     on_attach = on_attach,
 --     flags = lsp_flags,
 -- }
-require('lspconfig')['tsserver'].setup{
-    on_attach = on_attach,
-    flags = lsp_flags,
-}
-require('lspconfig')['rust_analyzer'].setup{
+
+require('lspconfig')['rust_analyzer'].setup {
     on_attach = on_attach,
     flags = lsp_flags,
     -- Server-specific settings...
-    cmd = {"/home/amirmohammad/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/bin/rust-analyzer"},
+    cmd = { "/home/amirmohammad/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/bin/rust-analyzer" },
     settings = {
-      ["rust-analyzer"] = {
-        imports = {
-          granularity = {
-            group = "module",
-          },
-          prefix = "self",
-        },
-        cargo = {
-          buildScripts = {
-            enable = true,
-          },
-        },
-        procMacro = {
-          enable = true
-        },
-      }
-  }
+        ["rust-analyzer"] = {
+            imports = {
+                granularity = {
+                    group = "module",
+                },
+                prefix = "self",
+            },
+            cargo = {
+                buildScripts = {
+                    enable = true,
+                },
+            },
+            procMacro = {
+                enable = true
+            },
+        }
+    }
 }
 
-local sumneko_root_path = os.getenv("HOME") .. "/.config/nvim/lua-language-server/"
-local sumneko_binary = sumneko_root_path .. "bin/lua-language-server"
-
-
-require("lspconfig")["sumneko_lua"].setup({
-    cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"},
-    capabilities = capabilities,
-    settings = {
-        Lua = {
-          runtime = {version = 'LuaJIT', path = vim.split(package.path, ';')},
-            completion = {enable = true, callSnippet = "Both"},
-            diagnostics = {
-                enable = true,
-                globals = {'vim', 'describe'},
-                disable = {"lowercase-global"}
-            },
-            workspace = {
-                library = {
-                    [vim.fn.expand('$VIMRUNTIME/lua')] = true,
-                    [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
-                    [vim.fn.expand('/usr/share/awesome/lib')] = true
-                    -- ,
-                    --[vim.fn.expand('/home/amirmohammad/.local/share/nvim/plugged/nvim-treesitter/lua/')] = true,
-                    --[vim.fn.expand('/home/amirmohammad/.local/share/nvim/plugged/nvim-treesitter/')] = true
-                },
-                -- adjust these two values if your performance is not optimal
-                maxPreload = 2000,
-                preloadFileSize = 1000
-            }
-        }
-    },
-    on_attach = on_attach,
-    flags = lsp_flags
-})
-
-
-require('lspconfig')['clangd'].setup{
+require('lspconfig')['clangd'].setup {
     on_attach = on_attach,
     flags = lsp_flags,
     root_dir = util.root_pattern('.clangd', '.clang-tidy', '.clang-format', 'compile_commands.json', 'compile_flags.txt', 'configure.ac', '.git'),
-    cmd = {"clangd-12"}
+    cmd = { "clangd-12" }
 }
 
-require("lspconfig")["bashls"].setup{
-    on_attach = on_attach,
-    flags = lsp_flags,
-    cmd = {"bash-language-server", "start"}
-}
+-- require("mason-lspconfig").setup_handlers({
+--     -- The first entry (without a key) will be the default handler
+--     -- and will be called for each installed server that doesn't have
+--     -- a dedicated handler.
+--     function(server_name) -- default handler (optional)
+--       require("lspconfig")[server_name].setup {
+--         on_attach = on_attach,
+--         flags = lsp_flags,
+--         capabilities = capabilities
+--       }
+--     end,
+--     -- Next, you can provide targeted overrides for specific servers.
+--     ["lua_ls"] = function()
+--       lspconfig.lua_ls.setup {
+--           settings = {
+--               Lua = {
+--                   diagnostics = {
+--                       globals = { "vim" }
+--                   }
+--               }
+--           }
+--       }
+--     end,
+-- })
 
 
 
+-- autocompletion with cmp
+
+-- Setup nvim-cmp.
+local cmp = require'cmp'
+
+cmp.setup({
+  snippet = {
+    -- REQUIRED - you must specify a snippet engine
+    expand = function(args)
+      vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+      -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+      -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+      -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+    end,
+  },
+  window = {
+    -- completion = cmp.config.window.bordered(),
+    -- documentation = cmp.config.window.bordered(),
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+  }),
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'vsnip' }, -- For vsnip users.
+    -- { name = 'luasnip' }, -- For luasnip users.
+    -- { name = 'ultisnips' }, -- For ultisnips users.
+    -- { name = 'snippy' }, -- For snippy users.
+  }, {
+    { name = 'buffer' },
+  })
+})
+
+-- Set configuration for specific filetype.
+cmp.setup.filetype('gitcommit', {
+  sources = cmp.config.sources({
+    { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
+  }, {
+    { name = 'buffer' },
+  })
+})
+
+-- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline('/', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = 'buffer' }
+  }
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(':', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = 'path' }
+  }, {
+    { name = 'cmdline' }
+  })
+})
+
+-- Setup lspconfig.
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
+-- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
