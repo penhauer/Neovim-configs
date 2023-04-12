fb493202e51741cdf6843ae1ce052736ba830192
amirmohammad ghasemi <amirmohammad.ghas3mi@gmail.com>
2023-04-12 17:49:08 +0330

    feat: remove help from TS languages

--- init.lua
+++ init.lua
@@ -185,7 +185,7 @@ require('telescope').setup{
 
 require('nvim-treesitter.configs').setup({
   -- A list of parser names, or "all" (the four listed parsers should always be installed)
-  ensure_installed = { "c", "lua", "vim", "help", "python" },
+  ensure_installed = { "c", "lua", "vim", "python" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
